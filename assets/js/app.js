2ecce5bf9d9675de71f9babf6770d14ef061c50d
telematica <ceron@maniak.com.mx>
2018-05-07 11:21:40 -0500

    Scrimba Lessons.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,17 @@ import _ from 'lodash';
 import '../scss/main.scss';
 //const css = require('../css/styles.css');
 
-function component() {
-	var element = document.createElement('div');
-	// Lodash, currently included via a script, is required for this line to work
-	// Lodash, now imported by this script
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function main() {
+    /*
+     var element = document.createElement('div');
+     // Lodash, currently included via a script, is required for this line to work
+     // Lodash, now imported by this script
+     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-	return element;
+     return element;
+     */
+    console.log(document, document.body, document.querySelector('body'));
+    //document.querySelector('body').appendChild(component());
 }
 
-
-window.onload = () => {
-	console.log(document, document.body, document.querySelector('body'));
-	//document.querySelector('body').appendChild(component());
-};
+window.onload = main;
